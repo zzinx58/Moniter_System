1306ddb02b16ee0af9a6af873ccf61627f2f1a14
凤之兮原 <1577310596@qq.com>
2022-07-31 22:01:33 +0800

    update: remove vitest to jest

---
+++ Main/src/__tests__/Test.spec.ts
@@ -0,0 +1,5 @@
+describe('test cases', () => {
+  it('1 + 1 shoule be 2', () => {
+    expect(1 + 1).toBe(2)
+  });
+});
---
+++ Main/src/views/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button data-tests="button">Button</button>
+</template>
+  
+<script setup lang='ts'>
+ 
+</script>
+  
+<style>
+  
+</style>
