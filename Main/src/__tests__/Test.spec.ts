7a0c15726cd88e04773352edeacc5764d00e7cf8
凤之兮原 <1577310596@qq.com>
2022-07-31 23:17:24 +0800

    chore: add vue/test-utills dependencies
    chore: add vue/test-utills dependencies

--- Main/src/__tests__/Test.spec.ts
+++ Main/src/__tests__/Test.spec.ts
@@ -1,5 +1,18 @@
-import { add } from "../views";
+import { mount } from '@vue/test-utils'
 
-it('1 + 1 shoule be 2, too', () => {
-  expect(add(1,1)).toBe(2)
+// The component to test
+const MessageComponent = {
+  template: '<p>{{ msg }}</p>',
+  props: ['msg']
+}
+
+test('displays message', () => {
+  const wrapper = mount(MessageComponent, {
+    props: {
+      msg: 'Hello world'
+    }
+  })
+
+  // Assert the rendered text of the component
+  expect(wrapper.text()).toContain('Hello world')
 })
