c8a893f2675a98e4348c2059254c8879603dc6a1
凤之兮原 <1577310596@qq.com>
2022-08-01 11:11:33 +0800

    chore: add first test-cases for Button.vue

--- Main/src/views/Button.vue
+++ Main/src/views/Button.vue
@@ -1,9 +1,11 @@
 <template>
-  <button data-tests="button">Button</button>
+  <button data-tests="button" @click="throwError">Button</button>
 </template>
   
 <script setup lang='ts'>
- 
+const throwError = () => {
+  throw new Error('Test error');
+} 
 </script>
   
 <style>
