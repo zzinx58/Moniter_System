8fbdacadf8bd71e82087c787f256b2f28e55257d
凤之兮原 <1577310596@qq.com>
2022-08-02 13:42:05 +0800

    remove: remvoe some not necessary code

--- Main/src/__tests__/jsError.spec.ts
+++ Main/src/__tests__/jsError.spec.ts
@@ -5,12 +5,10 @@ import Button from '@/views/Button.vue'
 
 let wrapper: VueWrapper<any>
 let button: DOMWrapper<any>
-let setupState: Record<string, unknown> = {}
 
 beforeAll(() => {
   wrapper = mount(Button as any)
   button = wrapper.find('[data-tests="button"]')
-  setupState = wrapper.vm.$.setupState
 })
 
 describe('first Button test', () => {
@@ -22,27 +20,6 @@ describe('first Button test', () => {
 
 describe('Button Cannot set property error of undefined', () => {
   it.skip('button injectJSError', async () => {
-    // const throwError = () => {
-    //   throw new Error(`Cannot set property 'error' of undefined`)
-    // }
-    // expect(() => throwError()).toThrowError("")
-    // const throwError = jest.fn()
-    // const Component = {
-    //   template: `<button @click="throwError"></button>`,
-    //   methods: {
-    //     throwError
-    //   }
-    // }
-    // const wrap = mount(Component, {})
-    // const fn = wrap.trigger('click')
-    // // await nextTick()
-    // expect(fn).rejects.toThrowError()
-    // expect(injectJSError()).toEqual({
-    //   "kind": "stability", // 大类
-    //   "type": "error", // 小类
-    //   "errorType": "jsError", // 错误类型
-    //   "filename": "http://localhost:5173/node_modules/.vite/deps/chunk-V4LB3WK7.js?v=59b8a3e7", // 访问的文件名
-    //   "position": "1427:7", // 行列信息
     const expectedErrorMessage =
       '[vue-test-utils]: you cannot set the target value of an event. See the notes section of the docs for more details—https://vue-test-utils.vuejs.org/api/wrapper/trigger.html'
 
@@ -62,21 +39,25 @@ describe('Button Cannot set property error of undefined', () => {
 
 
 
-// it('test button click handler', () => {
-//   const throwError = () => {
-//     try {
-//       throw new Error('thrown')
-//     } catch (e: any) {
-//       // console.log(e.message);
-//     }
-
-//   }
-//   const Component = {
-//     template: `<button @click="throwError"></button>`,
-//     methods: { throwError }
-//   }
-//   const wrap = mount(Component, {})
-//   const fn = wrap.trigger('click')
-//   expect(fn).resolves.toThrowError('thrown')
-// });
-
+it('test button click handler', (done) => {
+  let message: string = ''
+  const throwError = () => {
+    try {
+      throw new Error('thrown')
+    } catch (e: any) {
+      message = e.message
+    }
+  }
+  const Component = {
+    template: `<button @click="throwError"></button>`,
+    methods: { throwError }
+  }
+  const wrap = mount(Component, {})
+  const fn = wrap.trigger('click')
+  fn.then(() => {
+    if (message) {
+      expect(message).toBe('thrown')
+      done()
+    }
+  })
+});
