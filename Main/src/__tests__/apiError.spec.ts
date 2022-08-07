3643427fb1883a88463be74098adc610cffdd569
凤之兮原 <1577310596@qq.com>
2022-08-07 23:51:53 +0800

    change: change common function to async function

--- Main/src/__tests__/apiError.spec.ts
+++ Main/src/__tests__/apiError.spec.ts
@@ -2,8 +2,8 @@ jest.mock('@/test_compoents/demo')
 import { fetchData } from "@/test_compoents/demo";
 
 describe('api Test', () => {
-  it('should create a new instance', () => {
-    return fetchData().then(data => {
+  it('should return 123', async () => {
+    return await fetchData().then(data => {
       expect(eval(data)).toEqual('123')
     })
   })
