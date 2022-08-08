f4740ca94752f8d1126610b24ddfd6146bc0cf5b
凤之兮原 <1577310596@qq.com>
2022-08-08 11:48:25 +0800

    feat: add function for axios

--- Main/src/test_compoents/__MOCKS__/demo.ts
+++ Main/src/test_compoents/__MOCKS__/demo.ts
@@ -1,5 +1,28 @@
 export const fetchData = () => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     resolve("(function(){return '123'})()")
   })
+}
+export const axiosData = () => {
+  return new Promise((resolve) => {
+    resolve({
+      code: 200,
+      data: {
+        item: {
+          "msg": "lalala"
+        }
+      },
+      msg: "success"
+    })
+  }
+  )
+}
+export const axios404 = () => {
+  return new Promise((_, reject) => {
+    reject({
+      code: 404,
+      msg: "error"
+    })
+  }
+  )
 }
--- Main/src/test_compoents/demo.ts
+++ Main/src/test_compoents/demo.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 
-
 export const fetchData = () => {
   return axios.get('/').then(res => res.data)
 }
 
 
+export const axiosData = () => {
+  return axios.get('/success').then(res => res.data)
+}
 // '(function() {return '123'})())'
+
+export const axios200 = () => {
+  return axios.get('https://jsonplaceholder.typicode.com/todos/1')
+}
+export const axios404 = () => {
+  return axios.get('https://jsonplaceholder.typicode.com/todos/s')
+}
