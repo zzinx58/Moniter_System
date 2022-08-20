fecba2ae31a2f5bf0186fad0ba1fbb67081acacd
zzinx58 <q1095568627@163.com>
2022-08-20 10:05:43 +0800

    fix: fixed mock server problem

--- Main/mock/index.ts
+++ Main/mock/index.ts
@@ -1,6 +1,6 @@
 // import fg from "fast-glob";
 // const modules = await fg(["./**/*.(js|ts)"]);
-const modules = import.meta.glob("./**/*.(js|ts)");
+const modules = import.meta.glob("./**/*.(js|ts)", { eager: true });
 Object.keys(modules).forEach((it) => {
   modules[it];
 });
