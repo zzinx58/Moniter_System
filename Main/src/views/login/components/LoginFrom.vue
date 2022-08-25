668fb2cc817803b69c42c1f5631a37459bd131a0
zzinx58 <q1095568627@163.com>
2022-08-26 04:08:29 +0800

    feat: about  <Login/>

---
+++ Main/src/views/login/components/LoginFrom.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="form-wraper">
+    <div class="input-item">
+      <label>Username: </label>
+      <ElInput />
+    </div>
+    <div class="input-item">
+      <label>password: </label>
+      <ElInput show-password />
+    </div>
+    <div class="input-item">
+      <ElButton class="login-button" @click="handleLogin">LOGIN</ElButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+import { useRouter } from "vue-router";
+export default defineComponent({
+  name: "LoginForm",
+  setup() {
+    const router = useRouter();
+    const handleLogin = () => {
+      router.push("/index");
+    };
+    return {
+      handleLogin,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.form-wraper {
+  @apply flex-col mt-0;
+}
+@keyframes animate {
+  0% {
+    background-position: 0%;
+  }
+
+  100% {
+    background-position: 400%;
+  }
+}
+
+.login-button:hover {
+  color: #5dacbd;
+}
+
+.login-button {
+  @apply mt-8 font-bold;
+  // background-color: #5dacbd;
+  // background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  animation: animate 60s linear infinite;
+  background: linear-gradient(90deg, #03a9f4, #ebe295, #03a9f4);
+  background-size: 400%;
+  width: 100%;
+  height: 50px;
+}
+.input-item {
+  @apply mt-0 mx-auto;
+  ~ .input-item {
+    @apply mt-1;
+  }
+
+  width: 300px;
+  label {
+    color: #5dacbd;
+  }
+}
+</style>
---
+++ Main/src/views/login/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="page-container">
+    <div class="right"></div>
+    <div class="form-container">
+      <div class="logo-wrapper">
+        <FullLogo />
+      </div>
+      <div class="form-wrapper">
+        <LoginFrom />
+      </div>
+    </div>
+    <div class="actions-wrapper">
+      <Actions :showDarkModeSwitch="false" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  reactive,
+  onMounted,
+  onBeforeUnmount,
+} from "vue";
+import useAppConfigStore from "@/store/AppConfig";
+import useTOPOLOGY from "@/hooks/useTOPOLOGY";
+import { useZoomIn } from "@/hooks/useAnimate";
+export default defineComponent({
+  name: "Login",
+  setup() {
+    const appConfig = useAppConfigStore();
+    useTOPOLOGY(".right");
+    onMounted(() => {
+      useZoomIn("form-container");
+    });
+    return {
+      appConfig,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.form-container {
+  @apply absolute;
+  width: 45%;
+  height: 60%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(15, 29, 58, 0.85);
+  box-shadow: 8px 8px 32px 8px rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(8px);
+  -webkit-backdrop-filter: blur(8px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+
+.page-container {
+  @apply flex;
+  width: 100vw;
+  .right {
+    @apply flex-1;
+    background-color: #234384;
+  }
+  height: 100vh;
+}
+
+.actions-wrapper {
+  position: absolute;
+  // bottom: 0px;
+  top: 10px;
+  right: 10px;
+  color: #234384;
+}
+</style>
